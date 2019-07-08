0ccf141879c307bc64c734f15c53a060ae330125
Elisa Melendez <elisa.melendezfgkss@sv.cds>
2019-07-08 16:07:06 -0600

    Elisa

--- application/views/Inicio/empre.php
+++ application/views/Inicio/empre.php
@@ -1,7 +1,8 @@
 <!-- Área de las empresas para su fácil acceso a sus encuestas enlinea -->
 <!-------------------------------------------------------------------------------------------------------------------------------------------->
+<div class="container">
 <div class="col-xs-12 col-sm-6 col-md-8">
-	<div style=" text-align:center; margin:5% auto; width:40rem; heigth:40rem;" class="container-responsive">
+	<div style=" text-align:center; margin:5% auto; width:500%; heigth:40rem;" >
 		<div class="col-md-6">
 			<div class="card card-dark" style="background-color:2d4d49; border-color: #333;">
 				<div class="card-header">
@@ -9,11 +10,19 @@
 				</div>
 				<div class="card-body">
 					<div class="card-columns" style=" text-align:center; margin:1% auto; width:30rem; heigth:30rem;">
+						
 						<a href="">
-							<img src="<?= base_url('assets/images/empresas1.png') ?>" class="card-img-top" width="5%" heigth="5%" /><br>
+                            <img src="<?= base_url('assets/images/empresas1.png') ?>" class="card-img-top" width="5%" heigth="5%" /><br>
+                        </a>
+                        <a href="">
+                            <img src="<?= base_url('assets/images/empresas2.png') ?>" class="card-img-top" width="10%" heigth="10%" /><br>
 						</a>
+						<a href="">
+                            <img src="<?= base_url('assets/images/empresas3.png') ?>" class="card-img-top" width="10%" heigth="10%" /><br>
+                        </a>
 				</div>
 			</div>
+		</div>
 		</div>
 </div>
 <!-------------------------------------------------------------------------------------------------------------------------------------------->
