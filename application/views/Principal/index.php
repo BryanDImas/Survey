802aa78ac3c4228f5d814d341bf1075c3771ca0f
Bryan Dimas <bryan.dimasfgkss@sv.cds>
2019-07-09 11:26:18 -0600

    bryan

--- application/views/Principal/index.php
+++ application/views/Principal/index.php
@@ -11,59 +11,62 @@
 	<link href="<?= base_url() ?>assets/css/style.css" rel="stylesheet">
 </head>
 
-<body style="background-color:#212529">
-	<header></header>
-	<div class="container">
-		<div class="col-12">
+<body>
+	<header style="background-color:black; height:80px; color:white;"><br>
+		<div class="col-xs-12 col-sm-12 col-md-12">
+			<span style="display:inline-flex">
+				<h3><?= $encuesta->NombreEncuesta ?></h3>
+			</span>
+			<h4 class="float-right"><?= $encuesta->ObjetivoEncuesta ?></h4>
+		</div>
+	</header>
+	<div class="container-fluid" style="background-color:silver">
+		<div class="col-xs-12 col-sm-8 col-md-8 card"><br>
 			<form class="form-horizontal" method="POST" action="<?= base_url('PrincipalC/capturar/') ?>">
-				<fieldset>
-					<br>
-					<h1 class="text-center header"><?= $encuesta->NombreEncuesta ?></h1>
-					<!-- ======================================================================================================================= -->
-					<!-- Área del formulario -->
-					<!-- ======================================================================================================================= -->
-					<?php $num = 1;
-					foreach ($encuesta->preguntas as $pregunta) { ?>
-						<div class="form-group">
-							<h4 class="col-md-offset-1"><?= $num ?>.- <label for="respuesta"><?= $pregunta->Pregunta ?></label></h4>
-							<div class="">
-								<div class="radio">
-									<?php switch ($encuesta->IdFormato) {
-										case 1: ?>
+				<!-- ======================================================================================================================= -->
+				<!-- Área del formulario -->
+				<!-- ======================================================================================================================= -->
+				<div class="row">
+				<?php $num = 1;
+				foreach ($encuesta->preguntas as $pregunta) { ?>
+					<div class="form-group">
+						<p></p>
+						<h4 class="col-md-offset-1"><?= $num ?>.- <label for="respuesta"><?= $pregunta->Pregunta ?></label></h4>
+						<div class="">
+							<div class="radio">
+								<?php switch ($encuesta->IdFormato) {
+									case 1: ?>
+									<?php foreach ($pregunta->respuestas as $res) { ?>
+										<label><input type="radio" name="respuestas[<?= $pregunta->idPregunta ?>]" value="<?= $res['IdRespuestas'] ?>"> <?= $res['Respuestas'] ?></label>
+									<?php  } ?>
+									<?php break;
+								case 2: ?>
+									<?php foreach ($pregunta->respuestas as $res) { ?>
+										<label><input type="checkbox" name="respuestas[]" value="<?= $res['IdRespuestas'] ?>"> <?= $res['Respuestas'] ?></label>
+									<?php  } ?>
+									<?php break; ?>
+								<?php case 7: ?>
+									<select name="respuestas[]" id="">
 										<?php foreach ($pregunta->respuestas as $res) { ?>
-											<label><input type="radio" name="respuestas[<?= $pregunta->idPregunta ?>]" value="<?= $res['IdRespuestas'] ?>"> <?= $res['Respuestas'] ?></label>
+											<option value="<?= $res['IdRespuestas'] ?>"><?= $res['Respuestas'] ?></option>
 										<?php  } ?>
 										<?php break;
-									case 2: ?>
-										<?php foreach ($pregunta->respuestas as $res) { ?>
-											<label><input type="checkbox" name="respuestas[]" value="<?= $res['IdRespuestas'] ?>"> <?= $res['Respuestas'] ?></label>
-										<?php  } ?>
-										<?php break; ?>
-									<?php case 7: ?>
-										<select name="respuestas[]" id="">
-											<?php foreach ($pregunta->respuestas as $res) { ?>
-												<option value="<?= $res['IdRespuestas'] ?>"><?= $res['Respuestas'] ?></option>
-											<?php  } ?>
-											<?php break;
-									} ?>
-									</select>
-								</div>
+								} ?>
+								</select>
 							</div>
-							<?php $num++;
-						} ?>
-					</div>
-<input type="hidden" name="idencuesta" value="<?= $encuesta->idEncuesta?>">
-
-					<div class="form-group">
-						<div class="col-md-12 text-center">
-							<input type="submit" class="btn btn-primary btn-lg" value="Listo">
 						</div>
-					</div>
-				</fieldset>
+						<?php $num++;
+					} ?>
+			</div>	
+			</div>
+				<input type="hidden" name="idencuesta" value="<?= $encuesta->idEncuesta ?>">
 			</form>
 		</div>
 	</div>
-	</div>
+	<div class="form-group">
+		<div class="col-md-12 text-center">
+			<input type="submit" class="btn btn-primary btn-lg" value="Listo">
+		</div>
 	</div>
 	<!-- ======================================================================================================= -->
 	<footer></footer>
