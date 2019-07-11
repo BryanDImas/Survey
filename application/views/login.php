0e239a02a10f486b54f365bc6e65a90e65c84fed
BryanDimas <dtbryanjosue@gmail.com>
2019-07-10 22:17:39 -0600

    bryan

--- application/views/login.php
+++ application/views/login.php
@@ -11,6 +11,9 @@
 	<link href="<?= base_url() ?>assets/node_modules/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 	<link rel="stylesheet" href="<?= base_url() ?>assets/css/styleII.css">
 	<link href="<?= base_url() ?>assets/css/pages/login-register-lock.css" rel="stylesheet">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
 
 </head>
 
@@ -92,7 +95,7 @@
 				<!-- ============================================================== -->
 				<div class="row">
 					<div class="col-md-12 call-to-action" style="background-color:none">
-						<div class="fix-width"  id="empresas"></div>
+						<div class="fix-width" id="empresas"></div>
 					</div>
 				</div>
 				<!-- ============================================================== -->
@@ -126,12 +129,12 @@
 	<!-- ============================================================== -->
 	<script type="text/javascript">
 		var baseUrl = "<?= base_url() ?>";
-	</script>
+		</script>
+		<script src="<?= base_url() ?>assets/js/Login.js"></script>
 	<script src="<?= base_url() ?>assets/node_modules/jquery/jquery.min.js"></script>
 	<!-- Bootstrap tether Core JavaScript -->
 	<script src="<?= base_url() ?>assets/node_modules/bootstrap/js/popper.min.js"></script>
 	<script src="<?= base_url() ?>assets/node_modules/bootstrap/js/bootstrap.min.js"></script>
-	<script src="<?= base_url() ?>assets/js/Login.js"></script>
 </body>
 
 </html>
