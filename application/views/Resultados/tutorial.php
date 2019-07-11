91380576475213a4489fab8e5af1e8130e37cac0
ElisaMelendez <50377229+ElisaMelendez@users.noreply.github.com>
2019-07-10 23:13:46 -0600

    Elisa

--- application/views/Resultados/tutorial.php
+++ application/views/Resultados/tutorial.php
@@ -1,8 +1,3 @@
-
-
-
-
-
 <!-- ============================================================== -->
 <!-- Container fluid  -->
 <!-- ============================================================== -->
@@ -54,7 +49,7 @@
             <p class="text-muted m-t-0">Acontinuacion encontrará videos, que le explicaran paso a paso como crear, almacenar, publicar y difundir sus encuestas de forma facil y rápida. </p>
             <div id="code1" class="collapse">
                 <div class="highlight">
-                    <pre>
+<pre>
     <code><span class="nt">&lt;div</span> <span class="na">class=</span><span class="s">"card"</span> <span class="na">style=</span><span class="s">"width: 20rem;"</span><span class="nt">&gt;</span>
       <span class="nt">&lt;img</span> <span class="na">class=</span><span class="s">"card-img-top"</span> <span class="na">src=</span><span class="s">"..."</span> <span class="na">alt=</span><span class="s">"Card image cap"</span><span class="nt">&gt;</span>
       <span class="nt">&lt;div</span> <span class="na">class=</span><span class="s">"card-body"</span><span class="nt">&gt;</span>
@@ -66,108 +61,45 @@
 </pre>
                 </div>
             </div>
-            <!-- Row -->
-            <div class="row ">
-                <!-- column -->
-                <div class="col-lg-3 col-md-6">
-                    <!-- Card -->
-                    <div class="card card-inverse card-info" style="background-color: yellow; border-color: #333;">
-                        <div class="card-header">
+        </div>
+    </div>
+
+
+    <!-- Row -->
+    <div>
+        <div class="row ">
+            <!-- column -->
+            <div class="col-lg-4 col-md-7">
+                <!-- Card -->
+                <div class="card card-inverse" style="background-color: yellow; border-color: #333;">
+                    <div class="card-header">
                         <h4 class=" text-white">Video 1</h4>
-                            <h5 class="card-title">Crear publicidad</h5>
-                            
-                        </div>
-                        <div class="card-body">
-                          
-                            <img class=" embed-responsive-item card-video video-responsive" style= align:center> <video controls src="<?= base_url() ?>assets/videos/Survey.mp4" width="310" height="180"></video>
-                            
-                            <!-- <p class="card-text">With supporting text below as a natural lead-in to additional content.</p> -->
-                            <!--  <a href="#" class="btn btn-primary">Go somewhere</a> -->
-                        </div>
+                        <h5 class="card-title">¿Cómo crear publicidad?</h5>
+                    </div>
+                    <div class="card-body">
+                        <img class=" embed-responsive-item card-video video-responsive" style=align:center> <video controls src="<?= base_url() ?>assets/videos/Survey.mp4" width="385" height="200"></video>
                     </div>
                 </div>
             </div>
+        </div>
 
-
-
-                    <!-- Row -->
-                    <div class="row">
-                    <div class="col-12 m-t-30">
-                        <h4 class="m-b-0">Card styles</h4>
-                        <p class="text-muted m-t-0 font-12">Cards include various options for customizing their backgrounds, borders, and color.<code>.card-primary info, warning, danger</code></p>
+        <div class="row ">
+            <!-- column -->
+            <div class="col-lg-4 col-md-7">
+                <!-- Card -->
+                <div class="card card-inverse" style="background-color:aqua; border-color: #333;">
+                    <div class="card-header">
+                        <h4 class=" text-white">Video 2</h4>
+                        <h5 class="card-title">¿Cómo crear publicidad?</h5>
                     </div>
-                    <div class="col-md-6">
-                        <div class="card card-inverse" style="background-color: #333; border-color: #333;">
-                            <div class="card-header">
-                                <h4 class="m-b-0 text-white">Card Title</h4></div>
-                            <div class="card-body" style="background-color:yellow">
-                                <h3 class="card-title">Special title treatment</h3>
-                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="card card-inverse card-info">
-                            <div class="card-header">
-                                <h4 class="m-b-0 text-white">Card Title</h4></div>
-                            <div class="card-body">
-                                <h3 class="card-title">Special title treatment</h3>
-                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                <a href="#" class="btn btn-inverse">Go somewhere</a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-md-6">
-                        <div class="card card-inverse card-primary">
-                            <div class="card-header">
-                                <h4 class="m-b-0 text-white">Card Title</h4></div>
-                            <div class="card-body">
-                                <h3 class="card-title">Special title treatment</h3>
-                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                <a href="#" class="btn btn-inverse">Go somewhere</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="card card-inverse card-danger">
-                            <div class="card-header">
-                                <h4 class="m-b-0 text-white">Card Title</h4></div>
-                            <div class="card-body">
-                                <h3 class="card-title">Special title treatment</h3>
-                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                <a href="#" class="btn btn-inverse">Go somewhere</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="card card-inverse card-warning">
-                            <div class="card-header">
-                                <h4 class="m-b-0 text-white">Card Title</h4></div>
-                            <div class="card-body">
-                                <h3 class="card-title">Special title treatment</h3>
-                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                <a href="#" class="btn btn-inverse">Go somewhere</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="card card-inverse card-success">
-                            <div class="card-header">
-                                <h4 class="m-b-0 text-white">Card Title</h4></div>
-                            <div class="card-body">
-                                <h3 class="card-title">Special title treatment</h3>
-                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                <a href="#" class="btn btn-inverse">Go somewhere</a>
-                            </div>
-                        </div>
+                    <div class="card-body">
+                        <img class=" embed-responsive-item card-video video-responsive" style=align:center> <video controls src="<?= base_url() ?>assets/videos/Survey.mp4" width="385" height="200"></video>
                     </div>
                 </div>
-                <!-- End Row -->
-
-
-                <!-- End Row -->
-                <!-- ============================================================== -->
-                <!-- End Page Content -->
-                <!-- ============================================================== -->
+            </div>
+        </div>
+    </div>
+<!-- End Row -->
+<!-- ============================================================== -->
+<!-- End Page Content -->
+<!-- ============================================================== -->
