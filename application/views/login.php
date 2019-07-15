ea226fb1febb078e4bbf9bbc3f0647d39abc55e7
Leandro Carpio <leandro.carpiofgkss@sv.cds>
2019-07-15 17:06:05 -0600

    leandro

--- application/views/login.php
+++ application/views/login.php
@@ -10,27 +10,27 @@
 	<title>Survey</title>
 	<script src="https://kit.fontawesome.com/12fe8082b5.js"></script>
 	<!-- <link rel="stylesheet" href="<?= base_url() ?>assets/css/styleII.css"> -->
-	<link href="<?= base_url() ?>assets/style.css" rel="stylesheet" type="text/css" />
+	<link href="<?= base_url() ?>assets/css/styleII.css" rel="stylesheet" type="text/css" />
 	<link href="<?= base_url() ?>assets/node_modules/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 	<link href="<?= base_url() ?>assets/css/pages/login-register-lock.css" rel="stylesheet">
 
 </head>
 
-<body>
-	<header class="container" style="background-color:white; width:100%">
+<body style="background-repeat: no-repeat; background-size:cover; background-image:url(<?= base_url() ?>assets/images/background/tec.jpg);  width:100%;">
+	<header class="container" >
 		<div class="">
 			<nav class="navbar navbar-expand-md navbar-light p-l-0">
 				<a class="navbar-brand" href="javascript:avoid(0)"><img src="<?= base_url() ?>assets/images/Logo.png" alt="Home" width="150" height="40" /></a>
 				<ul class="navbar-nav ml-auto stylish-nav">
 					<!-- Logo will be here -->
-					<div class="col-md-9">
-						<div class="login-box">
-							<div class="">
-								<a href='#' data-toggle='dropdown' class="top_bar_left clearfix  btn-sm text-uppercase m-t-4 btn btn-nue font-10 btn-rounded" >INICIAR SESION</a>
-								<div class='dropdown-menu' style='padding: 4%; padding-bottom: 2%; background: #3DFCF0; width: 80%'>
+					<div class="col-md-12 col-xs-5">
+						<div class="login-container-box">
+								<a href='#' data-toggle='dropdown' class="col-md-12   btn-sm text-uppercase m-t-5 btn btn-nue font-10 btn-rounded" >INICIAR SESION</a>
+								<div class='dropdown-menu' style='padding: 5%; padding-bottom: 3%; background: #2f3d4a; opacity:0.9;  width: 80%'>
 									<form class="form-horizontal form-material" id="loginform" action="<?= base_url() ?>login/validar" method="post" autocomplete="off">
 										<a href="javascript:void(0)" class="text-center db"><img src="<?= base_url() ?>assets/images/logo3.png" alt="Home" width="15%" /><span><img src="<?= base_url() ?>assets/images/text-logo.png" alt="Home" width="42%" /></span></a>
-										<div class="form-group m-t-40">
+										<br>
+										<div class="form-group">
 											<div class="col-xs-12">
 												<input name="usua" class="text-center form-control" type="text" placeholder="Correo">
 											</div>
@@ -40,8 +40,8 @@
 												<input name="clave" class="text-center form-control" type="password" placeholder="Contraseña">
 											</div>
 										</div>
-										<div class="form-group text-center m-t-20">
-											<div class="col-xs-4 col-md-6 offset-3">
+										<div class="form-group m-t-5">
+											<div class="col-xs-3 col-md-5 offset-1">
 												<button class="btn btn-nue text-uppercase btn-rounded">Ingresar</button>
 											</div>
 										</div>
@@ -50,22 +50,11 @@
 										</div>
 									</form>
 								</div>
-							</div>
 						</div>
 					</div>
 				</ul>
 			</nav>
 	</header>
-	<!-- ============================================================== -->
-	<!-- Page wrapper  -->
-	<!-- ============================================================== -->
-	<div id="main-wrapper">
-		<div class="page-wrapper">
-			<!-- ============================================================== -->
-			<!-- Container fluid  -->
-			<!-- ============================================================== -->
-			<div class="container-fluid" style="background-repeat: no-repeat; background-size:cover; background-image:url(<?= base_url() ?>assets/images/background/tec.jpg);  width:100%;"><br>
-				<!-- ============================================================== -->
 				<!-- Contenido del area de publicidad -->
 				<!-- ============================================================== -->
 				<div class="container-fluid">
@@ -77,7 +66,7 @@
 				<!--  barra verde -->
 				<!-- ============================================================== -->
 				<div class="container-fluid">
-					<div class="col-md-12" style="background-color:none">
+					<div class="col-md-10" style="background-color:none">
 						<div id="empresas"></div>
 					</div>
 				</div>
