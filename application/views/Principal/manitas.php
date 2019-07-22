f92a17a5dd377f4bb56a4c5d63a96f771c675ec5
Elisa Melendez <elisa.melendezfgkss@sv.cds>
2019-07-22 10:52:20 -0600

    Elisa

--- application/views/Principal/manitas.php
+++ application/views/Principal/manitas.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Survey</title>
     <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url() ?>assets/images/logo3.png">
-    <link rel="stylesheet" href="<?= base_url() ?>assets/css/manitas.css">
+    <link rel="stylesheet" href="<?= base_url('assets/css/manitas.css') ?>">
     <link href="<?= base_url() ?>assets/node_modules/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="<?= base_url() ?>assets/css/pages/card-page.css">
 
@@ -16,10 +16,9 @@
 <body>
     <div class="container">
         <div class="contenedor">
-
             <div class="card-header">
                 <div class="logo-responsive">
-                    <img src="<?= base_url() ?><?= $encuesta->logo ?>" alt="">
+                    <img src="<?= base_url() ?><?= $encuesta->logo ?>">
                 </div>
                 <h2 class="card-title">
                     <h2><?= $encuesta->NombreEncuesta ?></h2>
@@ -34,11 +33,11 @@
                         <h4 class="col-md-offset-1"><?= $num + 1 ?>.- <label for="respuesta"><?= $pregunta->Pregunta ?></label></h4>
                         <div class="card-deck">
                             <div class=" col-md-2 col-xs-2 offset-4">
-                                <label for="radio1<?= $pregunta->idPregunta ?>"><img src="<?= base_url('assets/images/icon/abajo.png') ?>" height="75"></label><br>
+                                <label for="radio1<?= $pregunta->idPregunta ?>"><img src="<?= base_url('assets/images/icon/abajo.png') ?>" ></label><br>
                                 <input id="radio1<?= $pregunta->idPregunta ?>" type="radio" name="respuestas[<?= $num ?>]" value="No me gusta">
                             </div>
                             <div class=" col-md-2 col-xs-2">
-                                <label for="radio2<?= $pregunta->idPregunta ?>"><img src="<?= base_url('assets/images/icon/arriba.png') ?>" height="75"></label><br>
+                                <label for="radio2<?= $pregunta->idPregunta ?>"><img src="<?= base_url('assets/images/icon/arriba.png') ?>" ></label><br>
                                 <input id="radio2<?= $pregunta->idPregunta ?>" type="radio" name="respuestas[<?= $num ?>]" value="Me gusta">
                             </div>
                         </div>
